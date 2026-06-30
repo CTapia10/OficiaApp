9cffdda3faecafb87b77c3efe4c5700d3fd7f4f2
CTapia10 <cristianalejandro_t@hotmail.com>
2026-06-29 22:43:38 -0300

    feat: configuracion inicial de arquitectura en capas y dominio base

---
+++ OficiaApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OficiaApp.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid ID { get; protected set; }
+    public DateTime FechaCreacion { get; protected set; }
+
+    protected BaseEntity()
+	{
+		ID = Guid.NewGuid();
+		FechaCreacion = DateTime.UtcNow;
+    }
+
+}
---
+++ OficiaApp.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OficiaApp.Domain.Common;
+
+namespace OficiaApp.Domain.Entities;
+
+public class User : BaseEntity
+{
+	public string Username { get; private set; }
+	public string PasswordHash { get; private set; }
+	public string Email { get; private set; }
+
+    public User(string username, string passwordHash, string email) : base()
+	{
+		Username = username;
+		PasswordHash = passwordHash;
+		Email = email;
+	}
+
+
+}
