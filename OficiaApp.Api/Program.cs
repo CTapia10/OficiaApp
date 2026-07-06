350d826b33825f99b098225930896749f08158f7
CTapia10 <cristianalejandro_t@hotmail.com>
2026-07-06 20:28:05 -0300

    feat(infrastructure): implement UserRepository and register in DI container

--- OficiaApp.Api/Program.cs
+++ OficiaApp.Api/Program.cs
@@ -1,5 +1,7 @@
 using OficiaApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using OficiaApp.Domain.Repositories;
+using OficiaApp.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
---
+++ OficiaApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using OficiaApp.Domain.Repositories;
+using OficiaApp.Domain.Entities;
+using OficiaApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace OficiaApp.Infrastructure.Repositories
+{
+
+    public class UserRepository : IUserRepository
+    {
+        
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
