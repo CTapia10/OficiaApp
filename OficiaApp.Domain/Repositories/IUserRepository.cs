4d73078868629038079d586972cf6ef6abec37eb
CTapia10 <cristianalejandro_t@hotmail.com>
2026-07-06 20:19:00 -0300

    feat: repository contract

---
+++ OficiaApp.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using OficiaApp.Domain.Entities;
+
+namespace OficiaApp.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task AddUserAsync(User user);
+    }
+}
