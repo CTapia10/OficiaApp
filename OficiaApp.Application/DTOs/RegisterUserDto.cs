be4970231b9e2600d47c75bc96e80d1189284900
CTapia10 <cristianalejandro_t@hotmail.com>
2026-07-06 22:06:08 -0300

    feat(application): add RegisterUserDto and IUserService contract

---
+++ OficiaApp.Application/DTOs/RegisterUserDto.cs
@@ -0,0 +1,3 @@
+﻿namespace OficiaApp.Application.DTOs;
+
+public record RegisterUserDto(string Username, string Email, string Password);
---
+++ OficiaApp.Application/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using OficiaApp.Application.DTOs;
+
+namespace OficiaApp.Application.Services
+{
+    public interface IUserService
+    {
+        Task RegisterUserAsync(RegisterUserDto registerUserDto);
+    }
+}
