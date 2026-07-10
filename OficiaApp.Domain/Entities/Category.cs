79c4669d0c701a2d9d2e77db2678fe1cd5277a01
CTapia10 <cristianalejandro_t@hotmail.com>
2026-07-10 14:03:36 -0300

    feat(domain): add bidirectional navigation for ProfessionalProfile in Category

--- OficiaApp.Domain/Entities/Category.cs
+++ OficiaApp.Domain/Entities/Category.cs
@@ -9,6 +9,8 @@ namespace OficiaApp.Domain.Entities
     {
         public string Name { get; private set; }
         public string Description { get; private set; }
+        private readonly List<ProfessionalProfile> _professionalProfiles = new List<ProfessionalProfile>();
+        public IReadOnlyCollection<ProfessionalProfile> ProfessionalProfiles => _professionalProfiles.AsReadOnly();
 
         public Category(string name, string description)
         {
